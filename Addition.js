0768c0bef44763aad5929116c093cc8c98953b74
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-11 17:06:58 +0800

    New code

--- Addition.js
+++ Addition.js
@@ -1,5 +1,24 @@
-let a = 100;
-let b = 50;
-let x = a + b;
-document.getElementById("result").innerHTML = x;
-//addition function
+var count = prompt("Choose an arithmetic method: \n1. Addition \n2. Subtraktion\n3. Multiplikation\n4. Division");
+var x = parseInt(prompt("Enter your first number", "0"));
+var y = parseInt(prompt("Enter your second number", "0"));
+var z = +x + +y;
+
+
+// The 4 basic math cases
+switch (count) {
+  case '1':
+    alert("Answer: " + z);
+    break;
+
+  case '2':
+    alert("Answer: " + (x - y));
+    break;
+
+  case '3':
+    alert("Answer: " + x * y);
+    break;
+
+  case '4':
+    alert("Answer: " + x / y);
+    break;
+}
