52a8e3a69842d03544401981b5ccc8e77348aa75
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-11 17:35:32 +0800

    Renamed file

---
+++ mathbasic.js
@@ -0,0 +1,24 @@
+var count = prompt("Choose an arithmetic method: \n1. Addition \n2. Subtraction\n3. Multiplication\n4. Division");
+var x = parseInt(prompt("Enter your first number", "0"));
+var y = parseInt(prompt("Enter your second number", "0"));
+var z = +x + +y;
+
+
+// The 4 basic math cases
+switch (count) {
+  case '1':
+    alert("Answer: " + z);
+    break;
+
+  case '2':
+    alert("Answer: " + (x - y));
+    break;
+
+  case '3':
+    alert("Answer: " + x * y);
+    break;
+
+  case '4':
+    alert("Answer: " + x / y);
+    break;
+}
